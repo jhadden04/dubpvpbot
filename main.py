288b9cd967bc6a71c4ab58a92bc2c3f08f930dd6
John Hadden <59323260+jhadden04@users.noreply.github.com>
2020-12-06 15:23:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import praw
 import time
-reddit = praw.Reddit(client_id='*',
-                     client_secret='*',
+reddit = praw.Reddit(client_id='ipJJ9wtXdbvFIA',
+                     client_secret='6qOxdazldz1ZoUl1R0TbT3EHxPrnBQ',
                      user_agent='a reddit instance',
-                     username='*',
-                     password='*')
+                     username='thickythatiana',
+                     password='Ddyslk33')
 
 def imageposter():
     n = 0
     while True:
 
-        subreddits = ["Johntesting","AmongUs", "gaming", "MobileGaming", "Shopping"] # you can add to this
+        subreddits = ["Johntesting","AmongUs", "gaming", "MobileGaming", "Shopping"] # you can add to this/change it
 
         titles = ["change this"]  # you must change this to your desired title
         reddit.validate_on_submit = True
-        text = ["change this"]  # you must change this
+        text = ["change this"]  # you must change this to the text
         subreddit = reddit.subreddit(subreddits[n])
         try:
             subreddit.submit(titles[0], text[0])
